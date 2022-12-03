257524713c9fd751f3c5eefb3e82e793a7300c69
mohamedbadr <78334225+mohamedbadrco@users.noreply.github.com>
2022-12-03 08:23:52 +0200

    Update func.go

--- func.go
+++ func.go
@@ -17,3 +17,18 @@ func pimage(dc *gg.Context, col color.Color, x, y int, k string) {
     dc.DrawString(k, float64(x),float64(y))
 	// dc.DrawString(k, float64(x+7),float64(y+7))
  }
+ func rpimage(dc *gg.Context, col color.Color, x, y int, k string) {
+    dc.Rotate(float64(3.14159))
+	
+	//aR, aG, aB, aA := col.RGBA()
+	//_ ,_,_= aR, aG, aB
+	//col = color.RGBA{74,246 ,38, uint8(aA >>  8) }
+	
+    dc.SetColor(col)
+    
+    dc.DrawString(k, float64(-x-14),float64(-y+14))
+    dc.DrawString(k, float64(-x-14),float64(-y+14))
+	// dc.DrawString(k, float64(x+7),float64(y+7))
+    dc.Rotate(float64(3.14159))
+   // print(float64(x))
+ }
